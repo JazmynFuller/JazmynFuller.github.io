a926d31084fffe54b0876f6fec4fa75ffd3fbeca
Jazmyn Fuller <a1jazmyn@gmail.com>
2020-03-28 02:25:27 -0400

    adding remote church project
    Learn how to code your first cool website with daily verses of encouragment

---
+++ myWebsite/my-script.js
@@ -0,0 +1,78 @@
+var d = new Date();
+var dayOfTheMonth = d.getDate();
+console.log(dayOfTheMonth)
+var todaysVerse = new Array();
+
+todaysVerse[0] = "<strong>Phillipians 4:6-7</strong><br>\"Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God. And the peace of God, which transcends all understanding, will guard your hearts and your minds in Christ Jesus.\" "
+todaysVerse[1] = "<strong>Ecclesiastes 4:9</strong><br> \"Two are better than one, because they have a good return for their labor: If either of them falls down, one can help the other up. But pity anyone who falls and has no one to help them up. Also, if two lie down together, they will keep warm. But how can one keep warm alone?\" "
+todaysVerse[2] = "<strong>1 Peter 4:8</strong><br> \"Above all, love each other deeply, because love covers over a multitude of sins.\" "
+todaysVerse[3] = "<strong>Psalm 139:13-14</strong><br> \" For you created my inmost being; you knit me together in my mother’s womb. I praise you because I am fearfully and wonderfully made; your works are wonderful, I know that full well.\" "
+todaysVerse[4] = "<strong>Jeremiah 29:11</strong><br> \" For I know the plans I have for you,” declares the Lord, “plans to prosper you and not to harm you, plans to give you hope and a future.\" "
+todaysVerse[5] = "<strong>Job 16:5</strong><br> \"But my mouth would encourage you, comfort from my lips will bring you relief.\" "
+todaysVerse[6] = "<strong>Colossians 3:1-2</strong><br> \"Since, then, you have been raised with Christ, set your hearts on things above, where Christ is, seated at the right hand of God. Set your minds on things above, not on earthly things.\"  "
+todaysVerse[7] = "<strong>James 1:5-6</strong><br> \"If you need wisdom, ask our generous God, and he will give it to you. He will not rebuke you for asking. But when you ask him, be sure that your faith is in God alone. Do not waver, for a person with divided loyalty is as unsettled as a wave of the sea that is blown and tossed by the wind.\"  "
+todaysVerse[8] = "<strong>Deuteronomy 31:6</strong><br> \"Be strong and courageous. Do not be afraid or terrified because of them, for the LORD your God goes with you; he will never leave you nor forsake you.\"  "
+todaysVerse[9] = "<strong>John 14:27</strong><br> \"Peace I leave with you;my peace I give to you. Not as the world gives do I give to you. Let not your hearts be troubled, neither let them be afraid.\"  "
+todaysVerse[10] = "<strong>Psalm 23:4</strong><br> \"Even though I walk through the valley of the shadow of death, I will fear no evil, for you are with me; your rod and your staff, they comfort me.\"  "
+todaysVerse[11] = "<strong>1 Corinthians 10:13</strong><br> \"No temptation has overtaken you that is not common to man. God is faithful, and he will not let you be tempted beyond your ability, but with the temptation he will also provide the way of escape, that you may be able to endure it.\"  "
+todaysVerse[12] = "<strong>2 Timothy 3:16-17</strong><br> \"All Scripture is God-breathed and is useful for teaching, rebuking, correcting and training in righteousness, 17 so that the servant of God[a] may be thoroughly equipped for every good work.\"  "
+todaysVerse[13] = "<strong>Exodus 14:14</strong><br> \"The Lord will fight for you; you need only to be still.\"  "
+todaysVerse[14] = "<strong>Esphesian 6:14-17</strong><br> \"Stand firm then, with the belt of truth buckled around your waist, with the breastplate of righteousness in place, 15and with your feet fitted with the readiness that comes from the gospel of peace. 16In addition to all this, take up the shield of faith, with which you can extinguish all the flaming arrows of the evil one. Take the helmet of salvation and the sword of the Spirit, which is the word of God.\"  "
+todaysVerse[15] = "<strong>Colossians 3:23-24</strong><br> \" Whatever you do, work heartily, as for the Lord and not for men, knowing that from the Lord you will receive the inheritance as your reward. You are serving the Lord Christ.\"  "
+todaysVerse[16] = "<strong>Romans 12:2</strong><br> \"Do not be conformed to this world, but be transformed by the renewal of your mind, that by testing you may discern what is the will of God, what is good and acceptable and perfect.\"  "
+todaysVerse[17] = "<strong>Proverbs 23:17</strong><br> \"Let not your heart envy sinners, but continue in the fear of the Lord all the day.\"  "
+todaysVerse[18] = "<strong>Psalm 119:2</strong><br> \" Blessed are those who keep his testimonies, who seek him with their whole heart.\"  "
+todaysVerse[19] = "<strong>Proverbs 16:3</strong><br> \" Commit your work to the Lord, and your plans will be established.\"  "
+todaysVerse[20] = "<strong>Psalm 66:20</strong><br> \"Blessed be God, because he has not rejected my prayer or removed his steadfast love from me!\"  "
+todaysVerse[21] = "<strong>James 5:16</strong><br> \"Therefore, confess your sins to one another and pray for one another, that you may be healed. The prayer of a righteous person has great power as it is working.\"  "
+todaysVerse[22] = "<strong>Psalms 19:14</strong><br> \"Let the words of my mouth and the meditation of my heart be acceptable in your sight, O Lord, my rock and redeemer.\"  "
+todaysVerse[23] = "<strong>James 1:2-4</strong><br> \"when troubles of any kind come your way, consider it an opportunity for great joy.  For you know that when your faith is tested, your endurance has a chance to grow. So let it grow, for when your endurance is fully developed, you will be perfect and complete, needing nothing.\"  "
+todaysVerse[24] = "<strong>Proverbs 15:4</strong><br> \"A gentle tongue is a tree of life, but perverseness breaks the spirit.\"  "
+todaysVerse[25] = "<strong>Joshua 1:8</strong><br> \"This Book of the Law shall not depart from your mouth, but you shall meditate on it day and night, so that you may be careful to do according to all that is written in it. For then you will make your way prosperous, and then you will have good success.\"  "
+todaysVerse[26] = "<strong>Proverbs 14:29</strong><br> \"Whoever is patient has great understanding, but one who is quick-tempered displays folly.\"  "
+todaysVerse[27] = "<strong>Luke 18:14</strong><br> \"I tell you that this man, rather than the other, wen home justified before God. For those who exalt themselves will be humbled , and those who humble themselves will be exalted.\"  "
+todaysVerse[28] = "<strong>1 Peter 2:2-3</strong><br> \"Like newborn babies, crave pure spiritual milk, so that by it you may grow up in your salvation; now that you gave tasted thaat the Lord is good.\"  "
+todaysVerse[29] = "<strong>1 John 4:8</strong><br> \"Whoever does not love goes not know God, because God is love.\"  "
+todaysVerse[30] = "<strong>Galations 2:20</strong><br> \"I have been crucified with Christ and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me.\"  "
+
+document.getElementById("verse-of-the-day").innerHTML = todaysVerse[dayOfTheMonth-1];
+
+
+
+function feelingGood(){
+	var randomNumber = Math.floor((Math.random() * 5));
+	var happy = new Array();
+	happy[0] = "Spiritual Reminder: <strong>Psalm 37:4</strong><br><br>\"Delight yourself in the Lord, and he will give you the desires of your heart.\"";
+	happy[1] = "Spiritual Reminder: <strong>Isaiah 55:12</strong><br><br>\"For you shall go out in joy and be led forth in peace; the mountains and the hills before you shall break forth into singing, and all the trees of the field shall clap their hands.\"";
+	happy[2] = "Spiritual Reminder: <strong>Acts 2:28</strong><br><br>\"You have made known to me the paths of life; you will make me full of gladness with your presence.\"";
+	happy[3] = "Spiritual Reminder: <strong>Esphesians 4:28</strong><br><br>\"Let the thief no longer steal, but rather let him labor, doing honest work with his own hands, so that he may have something to share with anyone in need.\"";
+	happy[4] = "Spiritual Reminder: <strong>Mark 16:15</strong><br><br>\"And he said to them, “Go into all the world and proclaim the gospel to the whole creation.\"";
+	console.log(randomNumber);
+	document.getElementById('encouraging-verse').innerHTML = happy[randomNumber];
+	// alert("Hello Happy Guy!")
+
+}
+
+function feelingMeh(){
+	var randomNumber = Math.floor((Math.random() * 5));
+	var meh = new Array();
+	meh[0] = "Spiritual Vitamin: <strong>Joshua 1:9</strong><br><br>\"Have I not commanded you? Be strong and courageous. Do not be frightened, and do not be dismayed, for the Lord your God is with you wherever you go.\"";
+	meh[1] = "Spiritual Vitamin: <strong>Deuteronomy 31:6</strong><br><br>\"Be strong and courageous. Do not fear or be in dread of them, for it is the Lord your God who goes with you. He will not leave you or forsake you.\"";
+	meh[2] = "Spiritual Vitamin: <strong>2 Timothy 1:7</strong><br><br>\"For God gave us a spirit not of fear but of power and love and self-control.\"";
+	meh[3] = "Spiritual Vitamin: <strong>Psalm 27:14</strong><br><br>\"Wait for the Lord; be strong, and let your heart take courage; wait for the Lord!\"";
+	meh[4] = "Spiritual Vitamin: <strong>Psalm 112:7</strong><br><br>\"He is not afraid of bad news; his heart is firm, trusting in the Lord.\"";
+	document.getElementById('encouraging-verse').innerHTML = meh[randomNumber];
+	// alert("Hello Meh Guy!")
+}
+
+function feelingSad(){
+	var randomNumber = Math.floor((Math.random() * 5));
+	var sad = new Array();
+	sad[0] = "Spiritual Medicine: <strong>Phillipians 4:8</strong><br><br>\"Finally, brothers and sisters, whatever is true, whatever is noble, whatever is right, whatever is pure, whatever is lovely, whatever is admirable—if anything is excellent or praiseworthy—think about such things.\"";
+	sad[1] = "Spiritual Medicine: <strong>Psalm 34:17</strong><br><br>\"The righteous cry out, and the LORD hears them; he delivers them from all their troubles.\"";
+	sad[2] = "Spiritual Medicine: <strong>1 Peter 5:6-7</strong><br><br>\"Humble yourselves, therefore, under God’s mighty hand, that he may lift you up in due time. 7 Cast all your anxiety on him because he cares for you.\"";
+	sad[3] = "Spiritual Medicine: <strong>Isaiah 41:10</strong><br><br>\"So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand.\"";
+	sad[4] = "Spiritual Medicine: <strong>Romans 8:38-39</strong><br><br>\"For I am convinced that neither death nor life, neither angels nor demons, neither the present nor the future, nor any powers, neither height nor depth, nor anything else in all creation, will be able to separate us from the love of God that is in Christ Jesus our Lord.\"";
+	document.getElementById('encouraging-verse').innerHTML = sad[randomNumber];
+	// alert("Hello Sad Guy!")
+}
